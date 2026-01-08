588ba08bb17a5cd47cd0806ae5ce8a94c2b52da8
Stephen Hawes <sphawes@gmail.com>
2026-01-08 11:07:56 -0500

    updates peel time per tenth mm

--- photon/src/PhotonFeeder.cpp
+++ photon/src/PhotonFeeder.cpp
@@ -40,7 +40,7 @@
 // --------
 
 // how long the peel motor will peel film per tenth mm of tape requested to be driven
-#define PEEL_TIME_PER_TENTH_MM 18
+#define PEEL_TIME_PER_TENTH_MM 22
 // how long the peel motor will peel film during backwards movements per tenth mm of tape requested to be driven
 #define BACKWARDS_PEEL_TIME_PER_TENTH_MM 30
 // short amount of time peel motor moves backwards to reduce tension on film after peeling
